a5decd8055f13c8732b8c9e45b5e604092e0972c
Janice Lam <janice.lam21@gmail.com>
2019-07-15 16:27:02 -0700

    proxy server running

--- server/app.js
+++ server/app.js
@@ -1,7 +1,37 @@
 
 const express = require('express');
+const path = require('path');
+const httpProxy = require('http-proxy');
 
 const app = express();
+const apiProxy = httpProxy.createProxyServer();
 app.use(express.static(path.join(__dirname, '/../public/')));
 
+ const serverOne = 'http://localhost:3001',
+       serverTwo = 'http://localhost:3002',
+       serverThree = 'http://localhost:3003',
+       serverFour = 'http://localhost:3004';
+
+app.all("/:restaurant_id/images", function(req, res) {
+    console.log('redirecting to Server1');
+    apiProxy.web(req, res, {target: serverOne});
+});
+
+app.all("/:restaurant_id/reservations", function(req, res) {
+    console.log('redirecting to Server2');
+    apiProxy.web(req, res, {target: serverTwo});
+});
+
+app.all("/:restaurant_id/menus", function(req, res) {
+    console.log('redirecting to Server3');
+    apiProxy.web(req, res, {target: serverThree});
+});
+
+app.all("/:restaurantID/reviews", function(req, res) {
+    console.log('redirecting to Server4');
+    apiProxy.web(req, res, {target: serverFour});
+});
+
 module.exports = app;
+
+
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = 3010;
+const port = 3000;
 
 module.exports = app.listen(port, () => {
   console.log(`listening on port ${port}...`);
