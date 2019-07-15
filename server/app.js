fb72d5860435fc65559cf2a5c9ffec142b76fea6
Janice Lam <janice.lam21@gmail.com>
2019-07-15 11:29:49 -0700

    modify proxy server setup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
+
 const express = require('express');
 
-const path = require('path');
-app.use(express.json());
+const app = express();
+app.use(express.static(path.join(__dirname, '/../public/')));
 
-app.use(express.static(path.join(__dirname, '../public')));
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const app = require('./app');
 
-const app = express();
 const port = 3010;
 
-app.listen(port, () => { console.log(`Listening on port ${port}`); });
+module.exports = app.listen(port, () => {
+  console.log(`listening on port ${port}...`);
+});
